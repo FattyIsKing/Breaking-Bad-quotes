790edf8b53ccd1790cd36a0936475ded699c4ea3
GrubyIsKing <95412743+GrubyIsKing@users.noreply.github.com>
2022-10-15 20:14:53 +0200

    Update

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,10 +1,13 @@
 const watch = document.querySelector(".watch");
 const returnTo = document.querySelector(".return");
+const left = document.querySelector(".left_half");
 
 watch.addEventListener("click", ()=>{
     right.style.display = "flex";
+    left.style.display = "none";
 });
 
 returnTo.addEventListener("click", ()=>{
     right.style.display = "none";
+    left.style.display = "flex";
 });
